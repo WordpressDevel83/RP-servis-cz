832b860bd15df00c93694291a1926486a1a65322
BMWdejf <167582053+WordpressDevel83@users.noreply.github.com>
2024-04-23 00:22:47 +0200

    Install clean Wordpress theme
    RPservis

---
+++ wp-content/themes/rpservis/footer.php
@@ -0,0 +1,12 @@
+<!-- Footer -->
+<footer id="footer" class="footer bg-overlay">
+
+</footer><!-- Footer end -->
+
+
+  <!-- Javascript Files
+  ================================================== -->
+</div><!-- Body inner end -->
+</body>
+
+</html>
